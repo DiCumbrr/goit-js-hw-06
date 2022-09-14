ef5d52d8ff5e0cdbd332c4e8c2536d3f045b80a0
Diana Kondratchuk <dianakondratchuk@gmail.com>
2022-09-14 17:50:43 +0300

    coplited the tasks

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+const liItemEl = document.querySelectorAll('.item')
+
+const numberOfCategories = liItemEl.length
+console.log(`Number of categories: ${numberOfCategories}`)
+
+const information = liItemEl.forEach(elem => {
+  console.log('')
+
+  const title = elem.firstElementChild.textContent
+  console.log(`Category: ${title} `)
+
+  const elemLength = elem.lastElementChild.children.length
+  console.log(`Elements: ${elemLength} `)
+})
